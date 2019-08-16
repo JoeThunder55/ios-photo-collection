628830e7f366bf30f43ea32361d432473df607a0
Aaron Haughton <AaronJHaughton@outlook.com>
2019-08-15 22:05:00 -0400

    Initial Commit

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Aaron on 8/15/19.
+//  Copyright © 2019 AlphaGrade, INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
